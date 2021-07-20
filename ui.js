7fe8a8bfab919bf9a28b46869f30da86712fae90
moutella <joaogabrielmsc@gmail.com>
2021-07-20 12:04:06 -0300

    Updates

--- ui.js
+++ ui.js
@@ -1,18 +1,1 @@
-document.addEventListener('DOMContentLoaded', function () {
-  var elems = document.querySelectorAll('.sidenav');
-  options = {
-    'edge': 'right'
-  }
-  var instances = M.Sidenav.init(elems, options);
-});
-
-console.log("hmmm")
-
-
-document.addEventListener('DOMContentLoaded', function () {
-  var elems = document.querySelectorAll('.fixed-action-btn');
-  options = {
-    'direction': 'top'
-  }
-  var instances = M.FloatingActionButton.init(elems, options);
-});
+document.addEventListener("DOMContentLoaded",(function(){var n=document.querySelectorAll(".sidenav");options={edge:"right"},M.Sidenav.init(n,options)})),console.log("hmmm"),document.addEventListener("DOMContentLoaded",(function(){var n=document.querySelectorAll(".fixed-action-btn");options={direction:"top"},M.FloatingActionButton.init(n,options)}));
